1169378d39011e5b0ac36d7872850e51d9ca4dc4
ramdasj <ramdas.maaya@gmail.com>
2018-04-24 07:04:33 -0400

    initial commit to the repo
    addded go file with different helloworld functions

---
+++ hello_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+import "strings"
+
+func TestStrReverse(t *testing.T) {  
+    reversedString := strReverse("Hello World")
+    expectedString := "dlroW olleH"
+    if strings.Compare(reversedString,expectedString)!=0  {
+       t.Errorf("String Value Incorrect, Actual: %s, Expected: %s.", reversedString, expectedString)
+    }
+}
